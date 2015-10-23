d8511da4fd8ce697e52e5bcd7201cdc3d675ead5
juaniiie <becjuana@gmail.com>
2015-10-23 15:50:03 -0700

    Feature: add setPassword and validPassword methods to user schema

--- db/models/User.js
+++ db/models/User.js
@@ -1,12 +1,26 @@
 var mongoose = require('mongoose');
 var db = require('../db.js');
+var crypto = require('crypto');
 
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  username: String,
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String,
   password: String
 });
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 100, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function (password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 100, 64).toString('hex');
+
+  return this.hash === hash;
+};
 
 var User = mongoose.model('User', userSchema);
 
