88cb1c8cea617fec737892d0f0ba9f7494ded006
juaniiie <becjuana@gmail.com>
2015-10-23 23:14:24 -0700

    Feature: adds AuthController methods: register and logIn

--- client/app/controller/authController.js
+++ client/app/controller/authController.js
@@ -1,11 +1,36 @@
-app.controller('AuthController', ['Auth',
-function(Auth) {
-  this.user = {
-    username: null,
-    password: null
+app.controller('AuthController', ['$scope','$state','Auth',
+function($scope, $state, auth) {
+  $scope.user = {};
+
+  $scope.register = function() {
+    auth.register($scope.user).error(function(error) {
+      $scope.error = error;
+    }).then(function() {
+      //go to cookbook?
+      $state.go('home');
+    });
   };
 
-  this.createUser = function () {
-    Auth.createUser(this.user);
+  $scope.logIn = function() {
+    auth.logIn($scope.user).error(function(error) {
+      //for displaying errors later on
+      $scope.error = error;
+    }).then(function() {
+      //go to cookbook?
+      $state.go('home');
+    });
   };
 }]);
+
+//before auth refactor
+// app.controller('AuthController', ['Auth',
+// function(Auth) {
+//   this.user = {
+//     username: null,
+//     password: null
+//   };
+
+//   this.createUser = function () {
+//     Auth.createUser(this.user);
+//   };
+// }]);
