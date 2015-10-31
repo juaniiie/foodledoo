0c313981ef66444c03efbc22103512d328350bf6
juaniiie <becjuana@gmail.com>
2015-10-30 17:27:04 -0700

    Refactor: Nutrition directive controller and html code now dry Bug: Table appears in recipe view, not in add recipe view

--- client/app/directive/nutrition.js
+++ client/app/directive/nutrition.js
@@ -5,39 +5,38 @@ app.directive('nutrition', function() {
       nutrition: '='
     },
     link: function(scope) {
-      scope.$watchCollection('nutrition', function() {
-        scope.cholesterol = scope.nutrition.totalNutrients.CHOLE;
-        scope.cholesterol.quantity = Number(scope.cholesterol.quantity).toFixed(1);
-        scope.cholesterol.dailyPercent = scope.nutrition.totalDaily.CHOLE.quantity.toFixed(1);
+      console.log('scope.nutrition', scope.nutrition);
 
-        scope.calories = scope.nutrition.totalNutrients.ENERC_KCAL;
-        scope.calories.quantity = Number(scope.calories.quantity).toFixed(1);
-        scope.calories.dailyPercent = scope.nutrition.totalDaily.ENERC_KCAL.quantity.toFixed(1);
+      scope.nutrientsData = [];
+      //
+      scope.dictionary = {
+        CHOLE: 'Cholesterol',
+        ENERC_KCAL: 'Calories',
+        PROCNT: 'Protein',
+        NA: 'Sodium',
+        FAT: 'Fat',
+        CHOCDF: 'Carbs',
+        FIBTG: 'Fiber',
+        SUGAR: 'Sugar'
+      };
 
-        scope.protein = scope.nutrition.totalNutrients.PROCNT;
-        scope.protein.quantity = Number(scope.protein.quantity).toFixed(1);
-        scope.protein.dailyPercent = scope.nutrition.totalDaily.PROCNT.quantity.toFixed(1);
+      scope.modifyNutrients = function(nutrientObj, key) {
+        nutrientObj.label = scope.dictionary[key];
+        nutrientObj.quantity = Number(nutrientObj.quantity).toFixed(1);
+        if (scope.nutrition.totalDaily[key] === undefined) {
+          nutrientObj.dailyPercent = 'N/A';
+        } else {
+          nutrientObj.dailyPercent = scope.nutrition.totalDaily[key].quantity.toFixed(1);
+        }
+        // console.log(nutrientObj);
+        return nutrientObj;
+      };
 
-        scope.sodium = scope.nutrition.totalNutrients.NA;
-        scope.sodium.quantity = Number(scope.sodium.quantity).toFixed(1);
-        scope.sodium.dailyPercent = scope.nutrition.totalDaily.NA.quantity.toFixed(1);
-
-        scope.fat = scope.nutrition.totalNutrients.FAT;
-        scope.fat.quantity = Number(scope.fat.quantity).toFixed(1);
-        scope.fat.dailyPercent = scope.nutrition.totalDaily.FAT.quantity.toFixed(1);
-
-        scope.carbs = scope.nutrition.totalNutrients.CHOCDF;
-        scope.carbs.quantity = Number(scope.carbs.quantity).toFixed(1);
-        scope.carbs.dailyPercent = scope.nutrition.totalDaily.CHOCDF.quantity.toFixed(1);
-
-        scope.fiber = scope.nutrition.totalNutrients.FIBTG;
-        scope.fiber.quantity = Number(scope.fiber.quantity).toFixed(1);
-        scope.fiber.dailyPercent = scope.nutrition.totalDaily.FIBTG.quantity.toFixed(1);
-
-        scope.sugar = scope.nutrition.totalNutrients.SUGAR;
-        scope.sugar.quantity = Number(scope.sugar.quantity).toFixed(1);
-        // scope.sugar.dailyPercent = scope.nutrition.totalDaily.SUGAR.quantity.toFixed(1);
-      });
+      for (var key in scope.nutrition.totalNutrients) {
+        if (!!scope.dictionary[key]) {
+          scope.nutrientsData.push(scope.modifyNutrients(scope.nutrition.totalNutrients[key], key));
+        }
+      }
     },
     templateUrl: 'app/templates/nutrition.html'
   };
